68bd6efa5702acd11c4ecd3c6e1d98ed7eeb7ccd
Elder Luiz <elderzxvm99@gmail.com>
2020-09-28 23:28:38 -0300

    finished proj using mvc

---
+++ BookListMVC/Controllers/BooksController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        [BindProperty]
+        public Book Book { get; set; }
+        public BooksController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Upsert(int? id)
+        {
+            Book = new Book();
+            //create
+            if (id == null)
+                return View(Book);
+            //update
+            Book = _db.Books.FirstOrDefault(u => u.Id == id);
+            if (Book == null)
+                return NotFound();
+            return View(Book);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert()
+        {
+            if (ModelState.IsValid)
+            {
+                if (Book.Id == 0)
+                    _db.Books.Add(Book);
+                else
+                    _db.Books.Update(Book);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Book);
+        }
+
+
+        #region Api Calls
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Books.ToListAsync() });
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Books.FirstOrDefaultAsync(n => n.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Failed to delete" });
+            }
+            _db.Books.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Deleted successfully" });
+        }
+        #endregion
+    }
+}
---
+++ BookListMVC/wwwroot/js/bookList.js
@@ -0,0 +1,62 @@
+﻿var dataTable;
+$(document).ready(() => {
+    loadDataTable();
+});
+
+loadDataTable = () => {
+    dataTable = $('#DT_load').DataTable({
+        'ajax': {
+            'url': '/books/getall',
+            'type': 'GET',
+            'datatype': 'JSON'
+        },
+        'columns': [
+            { 'data': 'name', 'width': '20% ' },
+            { 'data': 'author', 'width': '20% ' },
+            { 'data': 'isbn', 'width': '20% ' },
+            {
+                'data': 'id',
+                'render': (data) => {
+                    return `<div class="text-center">
+                                <a  href="/books/Upsert?id=${data}" 
+                                    class="btn btn-success text-white" 
+                                    style="cursor:pointer;width:100px">
+                                Edit
+                                </a>
+                                &nbsp;
+                                <a  onclick=Delete("/books/Delete?id="+${data})
+                                    class="btn btn-danger text-white" 
+                                    style="cursor:pointer;width:100px">
+                                Delete
+                                </a>
+                            </div>`
+                }, 'width': '30%'
+            }
+        ], 'language': {
+            'emptyTable': 'no data found'
+        }, 'width': '100%'
+    })
+}
+
+Delete = (url) => {
+    swal({
+        title: 'Are you sure?',
+        text: 'please confirm',
+        icon: 'warning',
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete)
+            $.ajax({
+                type: 'delete',
+                url: url,
+                success: (data) => {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else toastr.error(data.message);
+                }
+            })
+    });
+}
